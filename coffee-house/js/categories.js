b54ebd430e6dd6e54a924293c4292be193244036
daryakolenchenko <daria.kolenchenko@gmail.com>
2025-10-09 12:50:41 +0200

    feat: add cards loading

--- coffee-house/js/categories.js
+++ coffee-house/js/categories.js
@@ -1,11 +1,19 @@
 ;(async () => {
   const buttons = document.querySelector('.menu__buttons-container')
   const catalog = document.querySelector('.menu__list')
+  const loadButton = document.querySelector('.menu__load-button')
 
   const products = await fetchProducts()
+  let currentCategory = 'coffee'
+  let isLoaded = false
 
-  addEventListener('resize', renderFirst)
-  renderFirst()
+  let renderProducts = []
+
+  window.addEventListener('resize', () => {
+    fillCatalog(currentCategory, false)
+  })
+
+  fillCatalog('coffee', true)
 
   async function fetchProducts() {
     const response = await fetch('./products.json')
@@ -24,20 +32,21 @@
         .split('')
         .map((letter, index) => (index === 0 ? letter.toLowerCase() : letter))
         .join('')
-      fillCatalog(category)
+      fillCatalog(category, true)
+      isLoaded = false
     })
   }
 
-  function renderFirst() {
-    fillCatalog('coffee')
-  }
+  loadButton.addEventListener('click', async () => {
+    loadProducts()
+    isLoaded = true
+  })
 
   function renderCard(card) {
     const cardHTML = document.createElement('div')
     cardHTML.classList.add('menu__card')
 
     const image = document.createElement('img')
-    //image.alt = card.category;
     image.classList.add('menu__card__img')
     image.style.backgroundImage = `url("${card.src}")`
 
@@ -68,12 +77,67 @@
     return cardHTML
   }
 
-  function fillCatalog(category) {
-    const categoryProducts = products
-      .filter((product) => product.category === category)
-      .map((product) => renderCard(product))
-    const oldCards = [...catalog.children]
-    oldCards.forEach((card) => card.remove())
-    categoryProducts.forEach((category) => catalog.append(category))
+  function fillCatalog(category, first) {
+    let categoryProducts = products.filter((product) => product.category === category)
+    if (first && window.innerWidth > 768) {
+      currentCategory = category
+      loadButton.style.display = 'none'
+
+      renderProducts = []
+      renderProducts = [...categoryProducts]
+
+      categoryProducts = categoryProducts.map((product) => renderCard(product))
+
+      const oldCards = [...catalog.children]
+      oldCards.forEach((card) => card.remove())
+
+      categoryProducts.forEach((category) => catalog.append(category))
+    } else if (first && window.innerWidth <= 768) {
+      currentCategory = category
+      categoryProducts.length > 4 ? (loadButton.style.display = 'flex') : (loadButton.style.display = 'none')
+
+      categoryProducts = categoryProducts.slice(0, 4)
+      renderProducts = []
+      renderProducts = [...categoryProducts]
+
+      categoryProducts = categoryProducts.map((product) => renderCard(product))
+
+      const oldCards = [...catalog.children]
+      oldCards.forEach((card) => card.remove())
+
+      categoryProducts.forEach((category) => catalog.append(category))
+    } else if (!first && window.innerWidth <= 768) {
+      if (isLoaded) {
+        return
+      }
+      if (renderProducts.length === 4) {
+        return
+      }
+      loadButton.style.display = 'flex'
+      renderProducts = renderProducts.slice(0, 4)
+
+      const lessProducts = renderProducts.map((product) => renderCard(product))
+
+      const oldCards = [...catalog.children]
+      oldCards.forEach((card) => card.remove())
+
+      lessProducts.forEach((product) => catalog.append(product))
+    } else if (!first && window.innerWidth > 768) {
+      isLoaded = false
+      loadProducts()
+    }
+  }
+
+  function loadProducts() {
+    let additionalProducts = products
+      .filter((product) => product.category === currentCategory)
+      .filter((product) => !renderProducts.some((renderProduct) => product.name === renderProduct.name))
+    if (additionalProducts.length === 0) {
+      return
+    }
+    loadButton.style.display = 'none'
+    renderProducts = [...renderProducts, ...additionalProducts]
+    additionalProducts = additionalProducts.map((product) => renderCard(product))
+    additionalProducts.forEach((category) => catalog.append(category))
   }
 })()
